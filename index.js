f2679ef77e64618fa77b8585ec9b21b211f4e455
Samwel Mpuyuk <148690527+Sammy0254@users.noreply.github.com>
2025-03-27 16:47:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
